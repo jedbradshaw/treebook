b44f535211e42288bbe1182f855e48c43f70d36f
Jed Bradshaw <jwb3913@gmail.com>
2013-08-05 20:43:29 -0600

    created a rails pp and added statuses

---
+++ app/views/statuses/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @status, :name, :status, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '70969f549ce1523a8efdad299daf7aa5720cabb08de36a98f936ab162b32f8be93476c1a2d58d03cab2a411fc6949a9981fa9ab24cf55f7a1b76bf32b2b8bd48'
